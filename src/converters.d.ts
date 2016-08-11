4d8ce6f5e56a52a0643678d59b5bac678010bbaa
Max Davidson <davidsonmax@gmail.com>
2016-08-11 19:33:12 +0200

    Fix incorrect TypeScript declarations

--- src/converters.d.ts
+++ src/converters.d.ts
@@ -1,13 +1,13 @@
 import { Type } from './interfaces';
 
 declare class Converter<T extends Type> {
-  constructor(type: T, options?: { cache?: boolean; });
+  constructor(type: T);
   decode(buffer: ArrayBuffer | ArrayBufferView, data?: any, startOffset?: number): any;
   encode(data: any): ArrayBuffer;
   encode<BufferType extends ArrayBuffer | ArrayBufferView>(data: any, buffer: BufferType, startOffset?: number): BufferType;
 }
 
-export function createConverter<T extends Type>(type: T): Converter<T>;
+export function createConverter<T extends Type>(type: T, options?: { cache?: boolean; }): Converter<T>;
 
 export function decode(type: Type, buffer: ArrayBuffer | ArrayBufferView, data?: any, startOffset?: number): any;
 export function encode(type: Type, data: any): ArrayBuffer;
