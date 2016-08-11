d29b49c2167571a1714ce9081cc40d5c6a70c09e
Max Davidson <davidsonmax@gmail.com>
2016-08-11 15:15:29 +0200

    Update typings

--- src/converters.d.ts
+++ src/converters.d.ts
@@ -1,5 +1,12 @@
 import { Type } from './interfaces';
 
+export class Converter<T extends Type> {
+  constructor(type: T, options?: { cache?: boolean; });
+  decode(buffer: ArrayBuffer | ArrayBufferView, data?: any, startOffset?: number): any;
+  encode(data: any): ArrayBuffer;
+  encode<BufferType extends ArrayBuffer | ArrayBufferView>(data: any, buffer: BufferType, startOffset?: number): BufferType;
+}
+
 export function decode(type: Type, buffer: ArrayBuffer | ArrayBufferView, data?: any, startOffset?: number): any;
 export function encode(type: Type, data: any): ArrayBuffer;
 export function encode<T extends ArrayBuffer | ArrayBufferView>(type: Type, data: any, buffer: T, startOffset?: number): T;
